ea4aed7b122263e2533f0d477237ab141cc687ea
Alberto Kitumba <alberkitumba@gmail.com>
2024-04-08 22:11:26 +0100

    Add message array and new avatar images

---
+++ src/utils/messages.ts
@@ -0,0 +1,70 @@
+export const messagesArray = [
+  {
+    id: Math.random().toString(36).slice(2),
+    name: "Company Name",
+    message:
+      "That's great, I can help you with that! What type of product are you...",
+    time: "9:45 AM",
+    avatar: require("@/assets/img-1.png"),
+  },
+  {
+    id: Math.random().toString(36).slice(2),
+    name: "Stephen Yustiono",
+    message:
+      "Nice. I don't know why people get all worked up about hawaiian pizza. I ...",
+    time: "10:30 AM",
+    avatar: require("@/assets/img-2.png"),
+  },
+  {
+    id: Math.random().toString(36).slice(2),
+    name: "Erin Steed",
+    message:
+      "(Sad fact: you cannot search for a gif of the word “gif”, just gives you gifs.)",
+    time: "11:15 AM",
+    avatar: require("@/assets/img-3.png"),
+  },
+  {
+    id: Math.random().toString(36).slice(2),
+    name: "Daisy Tinsley",
+    message: "Maybe email isn't the best form of communication.",
+    time: "12:00 PM",
+    avatar: require("@/assets/img-4.png"),
+  },
+  {
+    id: Math.random().toString(36).slice(2),
+    name: "Zach Friedman",
+    message:
+      "Tabs make way more sense than spaces. Convince me I'm wrong. LOL.",
+    time: "1:00 PM",
+    avatar: require("@/assets/img-5.png"),
+  },
+  {
+    id: Math.random().toString(36).slice(2),
+    name: "Kyle & Aaron",
+    message: "That's what I'm talking about!",
+    time: "2:00 PM",
+    avatar: require("@/assets/img-6.png"),
+  },
+  {
+    id: Math.random().toString(36).slice(2),
+    name: "Dee McRobie",
+    message:
+      "There's no way you'll be able to jump your motorcycle over that bus.",
+    time: "3:00 PM",
+    avatar: require("@/assets/img-7.png"),
+  },
+  {
+    id: Math.random().toString(36).slice(2),
+    name: "Gary Butcher",
+    message: "Nathan is a hater, you can totes make that jump. Do it. Do it.",
+    time: "4:00 PM",
+    avatar: require("@/assets/img-8.png"),
+  },
+  {
+    id: Math.random().toString(36).slice(2),
+    name: "David Turissini",
+    message: "Message preview",
+    time: "5:00 PM",
+    avatar: require("@/assets/img-9.png"),
+  },
+];
