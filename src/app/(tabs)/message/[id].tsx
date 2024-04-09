0d11b26e2b9b2595400059c047a563515c0c4e1a
Alberto Kitumba <alberkitumba@gmail.com>
2024-04-09 18:56:27 +0100

    Add header into conversation page

--- src/app/(tabs)/message/[id].tsx
+++ src/app/(tabs)/message/[id].tsx
@@ -4,24 +4,39 @@ import {
   ScrollView,
   TouchableOpacity,
   StyleSheet,
+  Image,
 } from "react-native";
-import { SquarePen } from "lucide-react-native";
-import { useLocalSearchParams } from "expo-router";
+import { ChevronLeft, SquarePen } from "lucide-react-native";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { colors } from "@/theme/colors";
+import { messagesArray } from "@/utils/messages";
 
 export default function Home() {
   const { id } = useLocalSearchParams();
+  const router = useRouter();
+  const userData = messagesArray.find((item) => item.id === id);
+
+  const handleBack = () => {
+    router.back();
+  };
+
+  if (!userData) {
+    handleBack();
+  }
+
   return (
     <ScrollView style={styles.container}>
-      <StatusBar style="inverted" backgroundColor={colors.zinc[100]} />
-      <View>
-        <Text style={{ fontSize: 24, fontWeight: "900" }}>Message {id}</Text>
-        <View>
-          <TouchableOpacity>
-            <SquarePen />
-          </TouchableOpacity>
+      <StatusBar style="inverted" backgroundColor={colors.zinc[200]} />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={handleBack} style={styles.headerBackButton}>
+          <ChevronLeft size={24} color={colors.zinc["600"]} />
+        </TouchableOpacity>
+        <View style={styles.headerUserInfo}>
+          <Image style={styles.headerUserAvatar} source={userData?.avatar} />
+          <Text style={styles.headerUserName}>{userData?.name}</Text>
         </View>
+        <View style={{ width: 40, height: 40 }} />
       </View>
     </ScrollView>
   );
@@ -29,8 +44,38 @@ export default function Home() {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 46,
     flex: 1,
+  },
+
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: colors.zinc["200"],
     padding: 16,
+    paddingTop: 46,
+  },
+  headerBackButton: {
+    width: 40,
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 40,
+  },
+  headerUserInfo: {
+    alignItems: "center",
+    gap: 4,
+  },
+  headerUserAvatar: {
+    width: 40,
+    height: 40,
+
+    borderRadius: 40,
+    resizeMode: "cover",
+  },
+  headerUserName: {
+    fontSize: 11,
+    lineHeight: 13,
+    color: colors.zinc["800"],
   },
 });
